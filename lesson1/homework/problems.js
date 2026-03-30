27b4befca22cb972349da38b7c38ecac662c645f
cloudpanda09 <cloudpanda09@gmail.com>
2026-03-30 02:06:34 +0000

    Finished HW 1

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,24 +1,29 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
-
-
+let lucky_number = 10
+console.log(lucky_number)
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
-
-
+let hours_of_sleep = 8
+console.log(hours_of_sleep)
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-
-
+let favorite_fruit = "strawberry"
+console.log("My favorite fruit is " + favorite_fruit)
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
-
-
-
+let city = "Seattle"
+let country = "US"
+console.log(city)
+console.log(country)
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
+let pet_name = "Coco"
+let pet_age = 7
+let pet_type = "Cavalier King Charles Spaniel"
+console.log("My pet's name is " + pet_name + ", who is " + pet_age + " years old. She is a " + pet_type + ".")
