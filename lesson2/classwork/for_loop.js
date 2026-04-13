692dfee782da44e9b2729d6e9673c63a37461605
cloudpanda09 <cloudpanda09@gmail.com>
2026-04-13 00:58:38 +0000

    Finished Lesson 4

---
+++ lesson2/classwork/for_loop.js
@@ -0,0 +1,29 @@
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+
+// We don't want to do that :)
+
+// for (start; condition; change)
+for (let i = 0; i < 5; i++) {
+    console.log("Hello world!");
+}
+
+for (let i = 0; i < 4; i++) { // i = 0, 1, 2, 3       Loop runs 4 times
+    console.log("Moo!")
+}
+
+let num = 6;
+for (let i = 0; i < num; i++) {  // loop runs 6 times
+    console.log("Bark!");
+}
+
+for (let i = 0; i < 5; i++) { // i goes from 0 - 4
+    console.log(i);
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i * 2);  // prints 0, 2, 4, 6, 8 (each value of i multiplied by 2)
+}
---
+++ lesson4/classwork/logical_operators.js
@@ -0,0 +1,47 @@
+const prompt = require("prompt-sync")();
+
+let age = Number(prompt("How old are you? "));
+let has_ticket = prompt("Do you have a movie ticket? (yes/no) ");
+
+// AND: both conditions must be true
+
+// Situation: What is required to enter a PG-13 movie
+if (age >= 13 && has_ticket === "yes") {
+    console.log("You can enter the PG-13 movie.");
+} else {
+    console.log("Sorry, you can't enter.");
+}
+console.log("Movie check is complete.");
+
+// OR: at least one condition is true
+
+let has_pass = prompt("Do you have a bus pass? (yes/no) ");
+let has_coins = prompt("Do you have coins to pay? (yes/no) ");
+
+if (has_pass === "yes" || has_coins === "yes") {
+    console.log("You can ride the bus.");
+} else {
+    console.log("You can't ride the bus.");
+}
+console.log("Bus check is complete.");
+
+// NOT: flips true to false and false to true   
+
+let homework_done = prompt("Did you do your homework? (yes/no) ");
+
+if (!(homework_done === "yes")) {
+    console.log("Go finish your homework!");
+} else {
+    console.log("Nice job! You're all done.")
+}
+console.log("Homework check complete.");
+
+// You can combine multiple logical operators.
+let finished_homework = true;
+let is_weekend = false;
+let parent_said_yes = true;
+
+// ! first, && second, || last
+if (finished_homework && (is_weekend || parent_said_yes)) {
+    console.log("You can play games.");
+}
